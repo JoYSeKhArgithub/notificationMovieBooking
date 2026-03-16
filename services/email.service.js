11b4cc06e938f19fab2e49e6c619973ec1f2d34a
Joysekhar Banerjee <bjoysekhar@gmail.com>
2026-03-16 23:08:03 +0530

    first commit

---
+++ services/email.service.js
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+
+const sendEmail = (userId,password)=>{
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: userId,
+            pass: password,
+        },
+    })
+
+    transporter.sendMail({
+        from: "freeh3961@gmail.com",
+        to: "bjoysekhar@gmail.com",
+        subject: 'Test email from nodemailer',
+        text: "Hei this is test email"
+    })
+
+}
+
+
+export default sendEmail
